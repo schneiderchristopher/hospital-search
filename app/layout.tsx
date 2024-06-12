caf702c6838e67cbddee946e79194ce2a5814f05
Christopher Schneider <schneiderchristopher16@gmail.com>
2024-06-12 15:24:19 -0300

    feat: Add StyledComponentsRegistry for managing styled components in layout.tsx (needed because html renders before styles gets applied)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import StyledComponentsRegistry from "./registry";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
         <div id="modal-root"></div>
       </body>
     </html>
